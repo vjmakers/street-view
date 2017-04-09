82be15bbb6bb1de882c6308ae140b8c9c0df6ff6
Aaditya Patwari <aadipatwari@gmail.com>
2017-04-09 18:48:56 +0800

    A lot of shit
    1. Added new Images
    2. Images follow 001 002 etc
    3. variables for panos are P001 P002 etc.
    4. Location variables don’t matter

--- data.js
+++ data.js
@@ -1,55 +1,93 @@
+
+
 var locations = {
   outsideVJC: { lat: 1.3065322, lng: 103.9207787 },
   // Apparently locations are not needed.
-  entrance:  { lat: 1.306611, lng: 103.9207418 },
-  garden: { lat: 1.306402, lng: 103.9207328 }
+  //005:  { lat: 1.306611, lng: 103.9207418 },
+  //006: { lat: 1.306402, lng: 103.9207328 }
 };
+
 
 var panoramas = {
   outsideVJC: undefined,
 
-  entrance: {
-    description: 'Victoria Junior College - Entrance',
+  
+  P005: {
+    description: 'Victoria Junior College - P005',
     links: [
-      [ 'outsideVJC', 70, 'Outside VJC' ],
-      [ 'concourse', 225, 'Concourse' ]
+      [ 'outsideVJC', 180],
+      [ 'P006', 280]
     ],
-    centerHeading: -55,
-    tileUrl: './images/lmaooo.jpg'
+    centerHeading: 70,
+    tileUrl: './images/005.jpg'
   },
 
-  concourse: {
-    description: 'Victoria Junior College - Concourse',
+  P006: {
+    description: 'Victoria Junior College - P006',
     links: [
-      [ 'entrance', 220, 'Entrance' ],
-      [ 'garden', 340, 'Garden' ]
+      [ 'P005', 50,],
+      [ 'P007', 250]
     ],
     centerHeading: 80,
-    tileUrl: './images/concourse.jpg'
+    tileUrl: './images/006.jpg'
   },
 
-  garden: {
-    description: 'Victoria Junior College - Garden',
+  P007: {
+    description: 'Victoria Junior College - P007',
     links: [
-      [ 'concourse', -30, 'Concourse' ],
-      [ 'outsidePT', 120, 'Outside the Performance Theatre' ]
+      [ 'P006', 120],
+      [ 'P008', 210]
     ],
-    centerHeading: -105,
-    tileUrl: './images/garden.jpg'
+    centerHeading: 30,
+    tileUrl: './images/007.jpg'
   },
 
-  outsidePT: {
-    description: 'Victoria Junior College - Outside the Performance Theatre',
+  P008: {
+    description: 'Victoria Junior College - P008',
     links: [
-      [ 'garden', 115, 'Garden' ]
+      [ 'P007', 75],
+      [ 'P009', -125]
     ],
-    centerHeading: -80,
-    tileUrl: './images/outside_pt.jpg'
+    centerHeading: -30,
+    tileUrl: './images/008.jpg'
+  },
+
+  P009: {
+    description: 'Victoria Junior College - P009',
+    links: [
+      [ 'P008', 185],
+      [ 'P010', -85]
+    ],
+    centerHeading: 40,
+    tileUrl: './images/009.jpg'
+  },
+
+  P010: {
+    description: 'Victoria Junior College - P010',
+    links: [
+      [ 'P009', 100],
+      [ 'P011', -90]
+    ],
+    centerHeading: 230,
+    tileUrl: './images/010.jpg'
+  },
+
+  P011: {
+    description: 'Victoria Junior College - P011',
+    links: [
+      [ 'P010', -25]
+    ],
+    centerHeading: 30,
+    tileUrl: './images/011.jpg'
   }
+  
+
+
+
 };
 
 var startingLink = {
-  pano: 'entrance',
+  pano: 'P005',
   heading: -110,
   description: 'VJC'
 };
