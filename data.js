d59a6332e37a61ff1aa7bf990ccb9deae9ca65f3
casualvisitor <dhruv1234shah@gmail.com>
2017-04-09 22:28:29 +0800

    trial
    25

--- data.js
+++ data.js
@@ -75,11 +75,96 @@ var panoramas = {
   P011: {
     description: 'Victoria Junior College - P011',
     links: [
-      [ 'P010', -25]
+      [ 'P010', 100],
+      [ 'P016', -100]
     ],
     centerHeading: 30,
     tileUrl: './images/011.jpg'
-  }
+  },
+
+  P016: {
+    description: 'Victoria Junior College - P016',
+    links: [
+      [ 'P011', 100],
+      [ 'P017', -180],
+      [ 'P018', 330]
+    ],
+    centerHeading: 70,
+    tileUrl: './images/016.jpg'
+  },
+
+  P017: {
+    description: 'Victoria Junior College - P017',
+    links: [
+      [ 'P016', 100]
+    ],
+    centerHeading: 80,
+    tileUrl: './images/017.jpg'
+  },
+
+  P018: {
+    description: 'Victoria Junior College - P018',
+    links: [
+      [ 'P017', 100],
+      [ 'P019', 360]
+    ],
+    centerHeading: 80,
+    tileUrl: './images/018.jpg'
+  },
+
+  P019: {
+    description: 'Victoria Junior College - P019',
+    links: [
+      [ 'P020', 100],
+      [ 'P018', -90]
+    ],
+    centerHeading: 80,
+    tileUrl: './images/019.jpg'
+  },
+
+  P020: {
+    description: 'Victoria Junior College - P020',
+    links: [
+      [ 'P019', 100],
+      [ 'P026', -90]
+    ],
+    centerHeading: 80,
+    tileUrl: './images/020.jpg'
+  },
+
+  P026: {
+    description: 'Victoria Junior College - P026',
+    links: [
+      [ 'P020', 100],
+      [ 'P027', -90]
+    ],
+    centerHeading: 80,
+    tileUrl: './images/021.jpg'
+  },
+
+  P022: {
+    description: 'Victoria Junior College - P022',
+    links: [
+      [ 'P021', 100],
+      [ 'P023', -90]
+    ],
+    centerHeading: 80,
+    tileUrl: './images/022.jpg'
+  },
+
+  P023: {
+    description: 'Victoria Junior College - P023',
+    links: [
+      [ 'P022', 100],
+      [ 'P024', -90]
+    ],
+    centerHeading: 80,
+    tileUrl: './images/023.jpg'
+  },
+  
+  
+
+  
   
 
 
@@ -87,7 +172,7 @@ var panoramas = {
 };
 
 var startingLink = {
-  pano: 'P005',
+  pano: 'P011',
   heading: -110,
   description: 'VJC'
 };
